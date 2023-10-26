1752d5d33779ec79ee968220cd4d9c2bf3d833e2
Jip Veerbeek <jip.veerbeek@hotmail.nl>
2023-10-26 15:05:36 +0200

    newer

--- api/routes/listRouter.js
+++ api/routes/listRouter.js
@@ -2,6 +2,6 @@ import express from "express";
 import getList from "../controllers/getList.js";
 const listRouter = express.Router();
 
-beerRouter.get('/list', getList);
+listRouter.get('/list', getList);
 
 export default listRouter;
